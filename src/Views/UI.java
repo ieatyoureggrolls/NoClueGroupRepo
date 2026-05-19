616aa982fab16bccd5a795d29dfec85947bbc365
stunbot11 <143148371+stunbot11@users.noreply.github.com>
2026-05-19 15:39:37 -0600

    Added some UI and setup intellij more
    Added some basic UI things to print user information. Also added documentation for the functions that have things in them in the UI class

--- src/Views/UI.java
+++ src/Views/UI.java
@@ -1,4 +1,55 @@
 package Views;
 
+import Models.User;
+
 public class UI {
+    //region inputs
+
+    /**
+     * Prompts the user to input their username and returns what they entered.
+     * @return String
+     */
+    public static String getUserName(){
+        return Console.getStringInput("Please enter your username:", false);
+    }
+
+    /**
+     * Prompts the user to input their password and returns what they entered.
+     * @return String
+     */
+    public static String getPassword(){
+        return Console.getStringInput("Please enter your password:", false);
+    }
+
+    /**
+     * Prompts the user to input their email and returns what they entered.
+     * @return String
+     */
+    public static String getEmail(){
+        return Console.getStringInput("Please enter your email:", false);
+    }
+    //endregion
+
+    //region print functions
+
+    /**
+     * prints the given user's username, password, and email in one line.
+     * @param user
+     */
+    public static void printUserInfo(User user){
+        Console.writeln(String.format("""
+                User: %s
+                Password: %s
+                Email: %s
+                """,  user.getUsername(), user.getPassword(), user.getEmail()));
+    }
+
+    public static void printBills(User user){
+
+    }
+
+    public static void printIncome(User user){
+
+    }
+    //endregion
 }
