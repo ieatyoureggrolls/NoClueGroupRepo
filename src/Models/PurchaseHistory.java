cb07260ccde3618b80834c65e848950a39b1c5d4
EthanPetree <ethan.james.petree@gmail.com>
2026-05-24 17:33:40 -0600

    Added calculateTotalBalance method to PurchaseHistory model

--- src/Models/PurchaseHistory.java
+++ src/Models/PurchaseHistory.java
@@ -4,4 +4,18 @@ import java.util.ArrayList;
 
 public class PurchaseHistory {
     public ArrayList<EntryData> history =  new ArrayList<>();
+
+    public double calculateTotalBalance(){
+        double balance = 0.0;
+
+        for (EntryData entry : history){
+            if (entry.isExpense()){
+                balance -= entry.getAmount();
+            } else {
+                balance += entry.getAmount();
+            }
+        }
+
+        return balance;
+    }
 }
